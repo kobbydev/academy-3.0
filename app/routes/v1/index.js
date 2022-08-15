51703d416a097cc83dfb06b477bdb482314efe9f
Mjakaba13 <malcolmakaba@gmail.com>
2022-08-15 11:59:41 +0000

    Update

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './auth';
+import userRoutes from './users/user';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use(userRoutes);
 
 export default router;
