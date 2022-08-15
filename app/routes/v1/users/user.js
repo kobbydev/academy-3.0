5d6aabfcda78abcdc92b3e5a9956427fcc2602f5
Mjakaba13 <malcolmakaba@gmail.com>
2022-08-15 22:28:41 +0000

    User signup and login

--- app/routes/v1/users/user.js
+++ app/routes/v1/users/user.js
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 import { joiForUser, joiForLogin } from '../../../middlewares/validation/user';
-import { ValidationMiddleware, AuthMiddleware, RoleMiddleware } from '../../../middlewares';
+import { ValidationMiddleware, AuthMiddleware } from '../../../middlewares';
 import { userSignUp, userLogIn } from '../../../controllers/User';
 import { checkForUser } from '../../../middlewares/user';
-import { AuthHelper } from '../../../utils/helpers';
+// import { AuthHelper } from '../../../utils/helpers';
 
 const router = Router();
 const { validate } = ValidationMiddleware;
-const { loginEmailValidator, comparePassword, createToken, authenticate } = AuthMiddleware;
-const { roleValueValidator } = RoleMiddleware;
-const { generateToken } = AuthHelper;
+const { loginEmailValidator, comparePassword } = AuthMiddleware;
+// const { roleValueValidator } = RoleMiddleware;
 
 router.post('/user-signup', validate(joiForUser), checkForUser, userSignUp);
 router.post(
